2acdad487e469783f3ee768df8ca55028b8d6b6e
Javier348solis <javiersolisretana@gmail.com>
2024-09-04 09:48:12 -0600

    Pages creation

---
+++ practica-login/src/components/Iniciosesi.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Iniciosesi() {
+  return (
+    <div>
+     <section className='big-space-form' id='big-space'>
+      <input type="text" name="" id="space-name" />
+      <input type="password" name="" id="space-pass" />
+      <input type="email" name="" id="space-mail" />
+      <button type='submit'>Login</button>
+     </section>
+    </div>
+  )
+}
+
+export default Iniciosesi
---
+++ practica-login/src/pages/Formregister.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Formregister() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Formregister
